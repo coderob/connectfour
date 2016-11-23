e76ff25620c46f0f7d8488684a2639342337d594
Robert Wilson <rwilson513@gmail.com>
2016-11-22 22:18:14 -0500

    Removed deprecated JS files.

--- app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var root_module_1 = require('./root.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(root_module_1.RootModule);
-//# sourceMappingURL=main.js.map
